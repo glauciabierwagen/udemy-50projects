87855e8c1bccdfc7962b459cafc2a35bd9108c3c
GlauciaBierwagen <galsilv@alumni.usp.br>
2022-07-05 10:02:53 +0200

    Update Dark Mode
    Adding css styles and Javascript dark mode

--- dark-mode/script.js
+++ dark-mode/script.js
@@ -3,6 +3,6 @@ const toggle = document.querySelector(".toggle");
 
 toggle.addEventListener("click", () => {
   container.classList.toggle("dark")
-    ? (toggle.firstElementChild.className = "far fa-moon")
-    : (toggle.firstElementChild.className = "far fa-sun");
+  ? (toggle.firstElementChild.className= "far fa-moon")
+  : (toggle.firstElementChild.className= "far fa-sun");
 });
